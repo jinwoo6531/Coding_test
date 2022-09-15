61b1124f455b87a030bff6cbb271723ab618830c
jinwoo6531 <jintakk6531@gmail.com>
2022-09-15 22:48:54 +0900

    9/15 3번까지 완료

---
+++ programmers/level1.js
@@ -0,0 +1,23 @@
+//1번 짝수/홀수
+
+//2번
+//123 = 6
+// function solution(n) {
+//   return;
+//   String(n)
+//     .split('')
+//     .reduce((acc, curr) => acc + +curr, 0);
+// }
+// solution(123);
+
+//3번 약수구하여 더하기
+// function solution(n) {
+//   let answer = 0;
+//   for (let i = 0; i <= n; i++) {
+//     if (n % i === 0) {
+//       answer += i;
+//     }
+//   }
+
+//   console.log(answer);
+// }
