40fae6c63dc16fc21a421d8cf13854c443c527af
jinwoo6531 <jintakk6531@gmail.com>
2022-09-18 13:00:58 +0900

    9/18

--- programmers/level1.js
+++ programmers/level1.js
@@ -33,14 +33,22 @@
 
 //5번 자연수 n을 뒤집어 각 자리 숫자를 원소로 가지는 배열 형태로 리턴해주세요. 예를들어 n이 12345이면 [5,4,3,2,1]을 리턴합니다.
 //12345 = [5,4,3,2,1]
-function solution(n) {
-  let str = n.toString();
-  let s = str.split('').reverse();
-  let arr = [];
-  for (let i = 0; i < s.length; i++) {
-    arr.push(Number(s[i]));
-  }
-  
-}
+// function solution(n) {
+//   let str = n.toString();
+//   let s = str.split('').reverse();
+//   let arr = [];
+//   for (let i = 0; i < s.length; i++) {
+//     arr.push(Number(s[i]));
+//   }
+// }
 
-solution(12345);
+//6번 대문자와 소문자가 섞여있는 문자열 s가 주어집니다. s에 'p'의 개수와 'y'의 개수를 비교해 같으면 True, 다르면 False를 return 하는 solution를 완성하세요.
+// 'p', 'y' 모두 하나도 없는 경우는 항상 True를 리턴합니다. 단, 개수를 비교할 때 대문자와 소문자는 구별하지 않습니다.
+// s = 'pPoooyY'
+// function solution(s) {
+//   return (
+//     s.toUpperCase().split('P').length === s.toUpperCase().split('Y').length
+//   );
+// }
+
+// solution('pPoooyY');
