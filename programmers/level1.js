65862dcc0d1f8c6cc32fa993862d1590e3648c32
jinwoo6531 <63627617+jinwoo6531@users.noreply.github.com>
2022-09-21 17:25:38 +0900

    Update level1.js

--- programmers/level1.js
+++ programmers/level1.js
@@ -72,3 +72,13 @@
 //   console.log(String(n).split('').sort().reverse().join(''));
 // }
 // solution(118372);
+
+
+//9번 함수 solution은 정수 x와 자연수 n을 입력 받아, x부터 시작해 x씩 증가하는 숫자를 n개 지니는 리스트를 리턴해야 합니다
+function solution(x, n) {
+    var answer = [];
+    for (let i = 1; i <= n; i++) {
+        answer.push(x*i)
+    }
+    return answer;
+}
