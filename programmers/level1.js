cf5df12e79368cc1efd54d20ca4b66ed3f225e26
jinwoo6531 <jintakk6531@gmail.com>
2022-10-10 15:01:10 +0900

    10/10

--- programmers/level1.js
+++ programmers/level1.js
@@ -258,5 +258,11 @@
 //   return Number(answer);
 // }
 
-//
-solution('one4seveneight');
+//내적
+function solution(a, b) {
+  let answer = 0;
+  for (let i = 0; i < a.length; i++) {
+    answer += a[i] * b[i];
+  }
+  return answer;
+}
